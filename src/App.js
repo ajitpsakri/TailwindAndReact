e838ae8f5da02331320612eabd1c1406f473885f
ajitpsakri <smartsakri@gmail.com>
2020-09-15 13:27:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './styles/main.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="h-64">
+      <div class="p-4 m-4 bg-green-600">
+        <h1 class="test-2xl font-bold text-white">At least this ill not copy lol</h1>
+      </div>
+      <div class="p-4 m-4 bg-green-300 h-full">
+        <h2 class="text-green-900">HI there</h2>
+        
+      </div>
     </div>
   );
 }
